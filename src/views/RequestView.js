6c9d8c8101c0feeab88438d685694bcc14cd972d
Drew Hoener <drewhoener@gmail.com>
2020-03-03 01:59:57 -0500

    spruced up icons a bit

--- src/views/RequestView.js
+++ src/views/RequestView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {ListItemSecondaryAction, ListItemText, TextField} from "@material-ui/core";
+import {ListItemSecondaryAction, ListItemText, TextField, useMediaQuery, useTheme} from "@material-ui/core";
 import {Search} from "@material-ui/icons";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
@@ -30,46 +30,61 @@ const useStyle = makeStyles(theme => ({
         width: '100%'
     },
     listItem: {
-        maxWidth: '30%'
+        [theme.breakpoints.down('sm')]: {
+            width: '69.69%',
+        },
+        [theme.breakpoints.up('md')]: {
+            maxWidth: '30%'
+        }
+    },
+    requestedText: {
+        [theme.breakpoints.down('sm')]: {
+            paddingLeft: theme.spacing(1)
+        }
     },
     listItemText: {
         fontWeight: 'bold'
+    },
+    requestIcon: {
+        transform: 'translateY(-40%)',
     }
 }));
 
 const data = require('../data/users');
-const UserList = ({classes}) => (
-    <List className={classes.list}>
-        {
-            Object.entries(data).map(([key, val]) => {
-                console.log(key);
-                console.log(val);
-                let {requestedBefore, lastRequested} = val;
-                console.log(requestedBefore);
-                let time = requestedBefore ? moment(lastRequested) : null;
-                console.log(time !== null ? time.calendar() : "");
-                return (
-                    <React.Fragment>
-                        <Divider/>
-                        <ListItem key={`${key.toLowerCase().replace(' ', '_')}-${val.employeeID}`}>
-                            <ListItemText className={classes.listItem} primary={key}
-                                          primaryTypographyProps={{className: classes.listItemText}}
-                                          secondary={`Employee ID: ${val.employeeID}`}/>
-                            {
-                                (requestedBefore && time !== null) ?
-                                    <ListItemText primary={`Last Requested`} secondary={time.calendar()}/> : null
-                            }
-                            <ListItemSecondaryAction>
-                                <Icon style={{color: val.state.color}}>{val.state.icon}</Icon>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                    </React.Fragment>
-                )
-            })
-        }
-        <Divider/>
-    </List>
-);
+const UserList = ({classes}) => {
+    let theme = useTheme();
+    let largeScreen = useMediaQuery(theme.breakpoints.up('md'));
+    return (
+        <List className={classes.list}>
+            {
+                Object.entries(data).map(([key, val]) => {
+                    let {requestedBefore, lastRequested} = val;
+                    let time = requestedBefore ? moment(lastRequested) : null;
+                    return (
+                        <React.Fragment key={`${key.toLowerCase().replace(' ', '_')}-${val.employeeID}`}>
+                            <Divider/>
+                            <ListItem>
+                                <ListItemText className={classes.listItem} primary={key}
+                                              primaryTypographyProps={{className: classes.listItemText}}
+                                              secondary={`Employee ID: ${val.employeeID}`}/>
+                                {
+                                    (requestedBefore && time !== null) ?
+                                        <ListItemText className={classes.requestedText}
+                                                      primary={`Requested ${largeScreen ? 'Review' : ''}`}
+                                                      secondary={time.calendar()}/> : null
+                                }
+                                <ListItemSecondaryAction className={classes.requestIcon}>
+                                    <Icon style={{color: val.state.color}}>{val.state.icon}</Icon>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        </React.Fragment>
+                    )
+                })
+            }
+            <Divider/>
+        </List>
+    );
+}
 
 export default function RequestView(props) {
     const classes = useStyle();
