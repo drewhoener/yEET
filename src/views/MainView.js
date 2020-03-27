232bfad48fdbf9b79a98368f7822a3e6f789062f
Drew Hoener <drewhoener@gmail.com>
2020-03-27 00:56:50 -0400

    Added lint option to npm run

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,10 +1,10 @@
-import ResponsiveNav from "../components/ResponsiveNav";
-import DummyView from "./DummyView";
-import React from "react";
-import {Redirect, Route, Switch} from 'react-router-dom';
-import {makeStyles} from "@material-ui/core/styles";
-import ReadReviewView from "./ReadReviewView";
-import RequestView from "./RequestView";
+import ResponsiveNav from '../components/ResponsiveNav';
+import DummyView from './DummyView';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import ReadReviewView from './ReadReviewView';
+import RequestView from './RequestView';
 
 const useStyle = makeStyles(theme => ({
     root: {
@@ -25,7 +25,7 @@ export default function MainView(props) {
             <ResponsiveNav/>
             <main className={classes.content}>
                 <Switch>
-                    {/*Redirect the '/' path to the home view*/}
+                    {/* Redirect the '/' path to the home view*/ }
                     <Redirect exact from='/' to='/home'/>
                     <Route path='/home'>
                         <div className={classes.toolbar}/>
