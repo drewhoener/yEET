626515984bdbe18df089d6f497ff49bd065e3ae6
acg <aidancgreenslade@gmail.com>
2020-03-28 12:19:12 -0400

    Added a container for each of the respond page categories

--- src/views/RespondView.js
+++ src/views/RespondView.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 
 const useStyle = makeStyles(theme => ({
     root: {
-        //display: 'flex',
-        padding: theme.spacing(30)
+        display: 'flex',
+        padding: theme.spacing(3)
     },
-    toolbar: theme.mixins.toolbar
+    toolbar: theme.mixins.toolbar,
+    paper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        padding: 8,
+        width: '100%'
+    }
 }));
 
-export default function RespondView(props) {
-    const classes = useStyle();
+const Pending = ({classes}) => {
     return (
-        <div className={classes.root}>
+        <Paper className={classes.paper}>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
             ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
             facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
@@ -24,6 +32,59 @@ export default function RespondView(props) {
             imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
             arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
             donec massa sapien faucibus et molestie ac.</p>
-        </div>
+        </Paper>
+    );
+};
+
+const Accepted = ({classes}) => {
+    return (
+        <Paper className={classes.paper}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.</p>
+        </Paper>
+    );
+};
+
+const Completed = ({classes}) => {
+    return (
+        <Paper className={classes.paper}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+            donec massa sapien faucibus et molestie ac.</p>
+        </Paper>
+    );
+};
+
+export default function RespondView(props) {
+    const classes = useStyle();
+    return (
+        <React.Fragment>
+            <div className={classes.toolbar}/>
+            <div className={classes.root}>
+                <Container maxWidth='xl'>
+                    <h1>Pending</h1>
+                    <Pending classes={classes}/>
+                    <h1>Accepted</h1>
+                    <Accepted classes={classes}/>
+                    <h1>Completed</h1>
+                    <Completed classes={classes}/>
+                </Container>
+            </div>
+        </React.Fragment>
     );
 }
