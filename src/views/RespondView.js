9f7cb5c56c35a872a8a93190979205ae92846614
acg <aidancgreenslade@gmail.com>
2020-03-28 16:32:00 -0400

    Added requests lists rendered based on the request status

--- src/views/RespondView.js
+++ src/views/RespondView.js
@@ -3,6 +3,13 @@ import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import {ListItemSecondaryAction, ListItemText, TextField, useMediaQuery, useTheme} from "@material-ui/core";
+import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
+import moment from "moment";
 
 const useStyle = makeStyles(theme => ({
     root: {
@@ -16,22 +23,71 @@ const useStyle = makeStyles(theme => ({
         alignItems: 'center',
         padding: 8,
         width: '100%'
+    },
+    margin: {
+        margin: theme.spacing(1)
+    },
+    list: {
+        width: '100%'
+    },
+    listItem: {
+        [theme.breakpoints.down('sm')]: {
+            width: '69.69%',
+        },
+        [theme.breakpoints.up('md')]: {
+            maxWidth: '30%'
+        }
+    },
+    listItemText: {
+        fontWeight: 'bold'
     }
 }));
+
+const users2 = require('../data/users2');
+const requests = require('../data/requestdata');
+const RequestList = ({classes, status}) => {
+    let theme = useTheme();
+    let reqs = requests.requests.filter(req => req.status == status);
+    return (
+        <List className={classes.list}>
+            {
+                reqs.map(req => { 
+                    console.log(reqs);
+                    let sender;                         // Get the request sender's data
+                    for (let user in users2.users) {    // There's probably a way to do this 10x less awful with the actual DB and mongoose (?)
+                        if (users2.users[user].employeeID == req.senderID) {
+                            sender = users2.users[user];
+                        }
+                    }
+                    return (
+                        <React.Fragment key={`${sender.name.toLowerCase().replace(' ', '_')}-${sender.employeeID}`}>
+                            <Divider/>
+                            <ListItem>
+                                <ListItemText tabIndex={0} primary={sender.name}
+                                              primaryTypographyProps={{className: classes.listItemText}}
+                                              secondary={sender.positionTitle}/>
+                                <ListItemSecondaryAction tabIndex={0}>
+                                    <IconButton aria-label={sender.state.name}
+                                                style={{color: sender.state.color}}><Icon>{sender.state.icon}</Icon></IconButton>
+                                </ListItemSecondaryAction>
+                                <ListItemSecondaryAction tabIndex={0}>
+                                    <IconButton aria-label={sender.state.name}
+                                                style={{color: sender.state.color}}><Icon>{sender.state.icon}</Icon></IconButton>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        </React.Fragment>
+                    )
+                })
+            }
+            <Divider/>
+        </List>
+    );
+};
 
 const Pending = ({classes}) => {
     return (
         <Paper className={classes.paper}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.</p>
+            <RequestList status={0} classes={classes}/>
         </Paper>
     );
 };
@@ -39,16 +95,7 @@ const Pending = ({classes}) => {
 const Accepted = ({classes}) => {
     return (
         <Paper className={classes.paper}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.</p>
+            <RequestList status={1} classes={classes}/>
         </Paper>
     );
 };
@@ -56,16 +103,7 @@ const Accepted = ({classes}) => {
 const Completed = ({classes}) => {
     return (
         <Paper className={classes.paper}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.</p>
+            <RequestList status={2} classes={classes}/>
         </Paper>
     );
 };
