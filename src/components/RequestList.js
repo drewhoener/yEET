a2df987b53801cff4fce456612d8fc02bf158a55
acg <aidancgreenslade@gmail.com>
2020-04-05 15:56:57 -0400

    Added mui-rte

--- src/components/RequestList.js
+++ src/components/RequestList.js
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import axios from 'axios';
 import Modal from '@material-ui/core/Modal';
 import { Close, Message, Send } from '@material-ui/icons';
+import MUIRichTextEditor from 'mui-rte'
 
 const buttons = {
     'accept': {
