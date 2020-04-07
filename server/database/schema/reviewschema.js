5753a0d3251f0105632e5d3af0ea90d0a235eb9c
Charlie Bouhanna <cbouhanna@umass.edu>
2020-04-06 22:01:22 -0400

    Finished ReviewSchema

--- server/database/schema/reviewschema.js
+++ server/database/schema/reviewschema.js
@@ -9,7 +9,18 @@ const reviewSchema = new Schema({
      * Date of when the review is published
      */
     dateWritten: { type: Date, required: true },
-
+    /**
+     * ID number for the specific review
+     */
+    reviewID: { type: Integer, required: true },
+    /**
+     * ID number for the specific request
+     */
+    requestID: { type: Integer, required: true },
+    /**
+     * Status on the completetion of the review
+     */
+    completed: { type: Boolean, required: true }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
