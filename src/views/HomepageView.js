197ab6604249f3c120f00c05dd79a281d3b138de
Charlie Bouhanna <cbouhanna@umass.edu>
2020-05-01 21:01:56 -0400

    Made HomepageView grab stats from the API

--- src/views/HomepageView.js
+++ src/views/HomepageView.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -31,13 +32,40 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+const MyStats = ({ classes }) => {
+    const [employeeId, setEmployeeId] = React.useState(null);
+    const [statsData, setStatsData] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get('/api/user-stats', {
+            params: {
+                _id: employeeId
+            }
+        })
+            .then( ({ data }) => {
+                console.log(data);
+                setStatsData(data);
+            })
+            .catch(err => {
+                console.log("Error in getting stats from API");
+            });
+
+    }, [employeeId]);
+
+    return (
+        <div>
+             Your Stats:
+        </div>
+    );
+};
+
 export default function HomepageView(props) {
     const classes = useStyles();
     return (
         <Container className={ classes.container }>
             <div className={ classes.paperContainer }>
                 <Paper className={ classes.paper }>
-                    Homepage container
+                    <MyStats classes = { classes }/>
                 </Paper>
             </div>
         </Container>
